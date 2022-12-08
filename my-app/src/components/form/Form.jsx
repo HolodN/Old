b9301c2578bdf0f448dc550f770af42d8a4698c6
HolodN <holodn@mail.ru>
2022-12-08 20:03:12 +0300

    Перерыв. Все работает.

--- my-app/src/components/form/Form.jsx
+++ my-app/src/components/form/Form.jsx
@@ -1,35 +1,45 @@
-import { useForm } from "react-hook-form"
-import React from "react"
+import { useForm } from "react-hook-form";
+import React from "react";
 import style from "./form.module.css"
+
 
 export default function Form(){
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState:{errors} } = useForm();
     const onSubmit = data => console.log(data);
-    
-return(
 
-<div>
-<form onSubmit={handleSubmit(onSubmit)}>
-    <h1>Заполните заявку на обратный звонок</h1>
-    <div>
-    <div>
-    <input
+    return(
+        <div className={style.form}>
+            <form id="form" onSubmit={handleSubmit(onSubmit)}>
+            <h1>Заполните заявку на обратный звонок</h1>
+            <div>
+                <div className="input-group mb-3"> 
+                     <input
                      {...register("lastName", {
                             required: true,
                             maxLength: 50,
                             pattern: /^[А-Яа-я]+$/i 
                      })}
-                        placeholder ="Фамилия"
-                     />
-    </div>
-    </div>
-</form>
-    
-</div>
-
-
-)
-
-
+                     type="text"
+                     className="form-control"
+                     placeholder="Фамилия"
+                    />
+                </div>
+                {errors?.lastName?.type === "required" &&(
+                    <p>Поле Фамилия обязательно для заполнения</p>
+                )}
+                {errors?.lastName?.type === "maxLength" &&(
+                    <p>Фамилия не может содержать более 50 символов</p>
+                )}
+                {errors?.lastName?.type === "pattern" &&(
+                    <p>Поле заполненно некорректно</p>
+                )}
+                
+            </div>
+            <div>
+                <input className="btn btn-outline-primary" type="submit"/>
+            </div>
+            </form>
+        </div>
+    )
 }
