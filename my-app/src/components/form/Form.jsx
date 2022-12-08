16f107b0e196de39cfdb089c14f65e43b6a574f5
HolodN <holodn@mail.ru>
2022-12-08 20:47:06 +0300

    Доработали форму заявки

--- my-app/src/components/form/Form.jsx
+++ my-app/src/components/form/Form.jsx
@@ -18,8 +18,7 @@ export default function Form(){
                      {...register("lastName", {
                             required: true,
                             maxLength: 50,
-                            pattern: /^[А-Яа-я]+$/i 
-                     })}
+                            pattern: /^[А-Яа-я]+$/i  })}
                      type="text"
                      className="form-control"
                      placeholder="Фамилия"
@@ -34,7 +33,65 @@ export default function Form(){
                 {errors?.lastName?.type === "pattern" &&(
                     <p>Поле заполненно некорректно</p>
                 )}
-                
+                <div className="input-group mb-3"> 
+                     <input
+                     {...register("firstName", {
+                            required: true,
+                            maxLength: 50,
+                            pattern: /^[А-Яа-я]+$/i 
+                     })}
+                     type="text"
+                     className="form-control"
+                     placeholder="Имя"
+                    />
+                </div>
+                {errors?.firstName?.type === "required" &&(
+                    <p>Поле Имя обязательно для заполнения</p>
+                )}
+                {errors?.firstName?.type === "maxLength" &&(
+                    <p>Имя не может содержать более 50 символов</p>
+                )}
+                {errors?.firstName?.type === "pattern" &&(
+                    <p>Поле заполненно некорректно</p>
+                )}
+                <div className="input-group mb-3"> 
+                     <input
+                     {...register("middleName", {
+                            maxLength: 50,
+                            pattern: /^[А-Яа-я]+$/i 
+                     })}
+                     type="text"
+                     className="form-control"
+                     placeholder="Отчество"
+                    />
+                </div>
+                {errors?.middleName?.type === "maxLength" &&(
+                    <p>Отчество не может содержать более 50 символов</p>
+                )}
+                {errors?.middleName?.type === "pattern" &&(
+                    <p>Поле заполненно некорректно</p>
+                )}
+                <div className="input-group mb-3"> 
+                     <input
+                     {...register("email", {
+                            required: true,
+                            maxLength: 50,
+                            pattern: /^[A-Za-z@._-]+$/i 
+                     })}
+                     type="text"
+                     className="form-control"
+                     placeholder="Email"
+                    />
+                </div>
+                {errors?.email?.type === "required" &&(
+                    <p>Поле Email обязательно для заполнения</p>
+                )}
+                {errors?.email?.type === "maxLength" &&(
+                    <p>Email не может содержать более 50 символов</p>
+                )}
+                {errors?.email?.type === "pattern" &&(
+                    <p>Поле заполненно некорректно</p>
+                )}
             </div>
             <div>
                 <input className="btn btn-outline-primary" type="submit"/>
